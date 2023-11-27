e3724c96403eea8209d4e32b50b49b17979f3e85
Deivid Santos <deividfranciscosantos@hotmail.com>
2023-11-27 13:18:39 -0300

    add categories data

--- introdução-processamento-linguagem-natural/t2-anon/ner-spacy.py
+++ introdução-processamento-linguagem-natural/t2-anon/ner-spacy.py
@@ -21,12 +21,18 @@ with open(caminho_arquivo, 'r') as file:
 sum_precision = 0
 total_count = 0
 
-for i in range(len(data)-2):
+categories_average = {}
+categories_average_count = {}
+
+for i in range(len(data)-1):
     annotator1_mentions = data[i]
 
     commons_entities = []
+    commons_entities_by_type = {}
+    spacy_entities_by_type = {}
     spacy_entities = []
     dataset_entities = []
+    grouped_dataset_entities = {}
     error_dataset_entities = []
     text = annotator1_mentions['text']
 
@@ -52,13 +58,18 @@ for i in range(len(data)-2):
 
     for mention in annotations['entity_mentions']:
         span_text = remove_stop_words(mention['span_text'])
-        # print('mention: ', mention['span_text'])
         if span_text in spacy_entities and span_text not in commons_entities:
             commons_entities.append(span_text)
+            if mention['entity_type'] not in commons_entities_by_type:
+                commons_entities_by_type[mention['entity_type']] = []
+            commons_entities_by_type[mention['entity_type']].append(span_text)
             similar_count += 1
             # print("Common", span_text)
         if span_text not in dataset_entities:
             dataset_entities.append(span_text)
+            if mention['entity_type'] not in grouped_dataset_entities:
+                grouped_dataset_entities[mention['entity_type']] = []
+            grouped_dataset_entities[mention['entity_type']].append(span_text)
         if span_text not in spacy_entities and span_text not in error_dataset_entities:
             error_dataset_entities.append(span_text)
             # print("Error Entity: ", span_text)
@@ -71,7 +82,24 @@ for i in range(len(data)-2):
     precision = similar_count * 100 / len(dataset_entities)
     sum_precision += precision
     total_count += 1
+    for item, value in grouped_dataset_entities.items():
+        dataset_size = len(value)
+        if item not in categories_average:
+            categories_average[item] = 0
+            categories_average_count[item] = 0
+        if (item in commons_entities_by_type): 
+            commons_size = len(commons_entities_by_type[item])
+            average = commons_size * 100 / dataset_size
+            # print('Category: ', item, ' Average: ', average)
+            categories_average[item] += average
+        else:
+            # print('Category: ', item, ' Average: ', 0)
+            categories_average[item] += 0
+        categories_average_count[item] += 1
     print("Precision: ", precision)
+
+for item, value in categories_average.items():
+    print('Complete category: ', item, ' Average: ', value / categories_average_count[item])
 
 print("Complete average: ", sum_precision/total_count)
 
--- introdução-processamento-linguagem-natural/t2-anon/ner-vladner.py
+++ introdução-processamento-linguagem-natural/t2-anon/ner-vladner.py
@@ -25,12 +25,18 @@ with open(caminho_arquivo, 'r') as file:
 sum_precision = 0
 total_count = 0
 
-for i in range(len(data)-2):
+categories_average = {}
+categories_average_count = {}
+
+for i in range(len(data)-1):
     annotator1_mentions = data[i]
 
     commons_entities = []
+    commons_entities_by_type = {}
+    vladner_entities_by_type = {}
     vladner_entities = []
     dataset_entities = []
+    grouped_dataset_entities = {}
     error_dataset_entities = []
     text = annotator1_mentions['text']
 
@@ -48,6 +54,10 @@ for i in range(len(data)-2):
         for result in ner_results:
             # print('result: ', result)
             clean_entity = remove_stop_words(result['word'])
+
+            if result['entity_group'] not in vladner_entities_by_type:
+                vladner_entities_by_type[result['entity_group']] = []
+            vladner_entities_by_type[result['entity_group']].append(clean_entity)
             vladner_entities.append(clean_entity)
             # print('clean entity: ', clean_entity)
 
@@ -62,12 +72,19 @@ for i in range(len(data)-2):
     for mention in annotations['entity_mentions']:
         span_text = remove_stop_words(mention['span_text'])
         # print('mention: ', span_text)
+        
         if span_text in vladner_entities and span_text not in commons_entities:
             commons_entities.append(span_text)
+            if mention['entity_type'] not in commons_entities_by_type:
+                commons_entities_by_type[mention['entity_type']] = []
+            commons_entities_by_type[mention['entity_type']].append(span_text)
             similar_count += 1
             # print("Common", span_text)
         if span_text not in dataset_entities:
             dataset_entities.append(span_text)
+            if mention['entity_type'] not in grouped_dataset_entities:
+                grouped_dataset_entities[mention['entity_type']] = []
+            grouped_dataset_entities[mention['entity_type']].append(span_text)
         if span_text not in vladner_entities and span_text not in error_dataset_entities:
             error_dataset_entities.append(span_text)
             # print("Error Entity: ", span_text)
@@ -76,12 +93,29 @@ for i in range(len(data)-2):
     # print("Dataset entities: ", len(dataset_entities))
     # print("Vladner entities: ", len(vladner_entities))
     # print("Common entities: ", similar_count)
-
     precision = similar_count * 100 / len(dataset_entities)
     sum_precision += precision
     total_count += 1
+
+    for item, value in grouped_dataset_entities.items():
+        dataset_size = len(value)
+        if item not in categories_average:
+            categories_average[item] = 0
+            categories_average_count[item] = 0
+        if (item in commons_entities_by_type): 
+            commons_size = len(commons_entities_by_type[item])
+            average = commons_size * 100 / dataset_size
+            # print('Category: ', item, ' Average: ', average)
+            categories_average[item] += average
+        else:
+            # print('Category: ', item, ' Average: ', 0)
+            categories_average[item] += 0
+        categories_average_count[item] += 1
     print("Precision: ", precision)
 
+for item, value in categories_average.items():
+    print('Complete category: ', item, ' Average: ', value / categories_average_count[item])
+   
 print("Complete average: ", sum_precision/total_count)
 
 # Complete average:  70.30070063211593
