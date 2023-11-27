58c72fa945932af1c5c4adbb70f75fb216ca6aa7
Deivid Santos <deividfranciscosantos@hotmail.com>
2023-11-27 10:53:00 -0300

    remove useless file

--- introdução-processamento-linguagem-natural/t2-anon/testvladner copy.py
+++
@@ -1,8 +0,0 @@
-from presidio_analyzer import AnalyzerEngine
-
-analyzer = AnalyzerEngine()
-
-results = analyzer.analyze(text="My phone number is 212-555-5555",
-                           entities=["PHONE_NUMBER"],
-                           language='en')
-print(results)
